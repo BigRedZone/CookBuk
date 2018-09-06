a32d18c47d3f30a6eccfb263268cdb96684ed1ff
Jhia Turner <jhimitu@gmail.com>
2018-09-06 16:17:40 -0700

    set up database/server connection

--- database/index.js
+++ database/index.js
@@ -1,1 +1,32 @@
 //database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/cookbook', { useNewUrlParser: true });
+
+const recipeSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  ingredients: String,
+  steps: String
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+const save = (recipe) => {
+  let newRecipe = new Recipe({
+    id: 00,
+    name: 'test name',
+    ingredients: 'test ingredients',
+    steps: 'test steps'
+  });
+
+  newRecipe.save((err, recipe) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('database connected');
+    }
+  });
+}
+
+module.exports.Recipe = Recipe;
+module.exports.save = save;
