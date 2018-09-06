1f23b1b7a7587cebf3aea430dfc7587caf9db675
Nathan Ong <nathan.d.ong@gmail.com>
2018-09-05 20:13:18 -0700

    Set up the server & database connections

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
 //server
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/test');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to Mongo!')
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(express.static(__dirname + '/../client/'));
+
+app.get('/', (req, res) => {
+  res.end('GET request initiated!');
+});
+
+app.post('/', (req, res) => {
+  res.end('POST request initiated!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}...`)
+});
