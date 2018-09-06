3e4e033ab5274acc09c5dd17532a953a52b40ba1
Nathan Ong <nathan.d.ong@gmail.com>
2018-09-06 14:28:24 -0700

    Remove db connection so that it can be created in database folder

--- server/index.js
+++ server/index.js
@@ -2,16 +2,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/test');
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to Mongo!')
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
