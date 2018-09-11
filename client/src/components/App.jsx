e144b1b22e9f42565a208421835123ec03fb7cc4
tae-lim <taelim92@gmail.com>
2018-09-11 09:48:00 -0700

    Merge with Vang

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
-
+import { GoogleLogin } from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 import AddRecipe from './AddRecipe.jsx';
 import Recipe from './Recipe.jsx';
 import Selection from './Selection.jsx';
@@ -14,13 +15,30 @@ class App extends React.Component {
     this.state = {
       view: '',
       recipeOTD: {},
+<<<<<<< Updated upstream
       recipe: {},
       recipes: sample
+=======
+      recipe: {
+        fullName: '',
+        name: 'Spaghetti',
+        ingredients: ['Linguini', 'Pasta Sauce', 'Ground Beef'],
+        steps: ['Add water to a pot', 'Heat pot until boiling', 'Add Linguini', 'Stir for 20 minutes', 'Drain water', 'Add Pasta Sauce', 'Add Ground Beef', 'Stir'],
+        cookTime: '21 minutes',
+        prepTime: '10 minutes',
+        servings: '2 servings'
+      }
+>>>>>>> Stashed changes
     };
 
     this.renderComponent = this.renderComponent.bind(this);
     this.changeView = this.changeView.bind(this);
     this.selectRecipe = this.selectRecipe.bind(this);
+<<<<<<< Updated upstream
+=======
+    this.addRecipe = this.addRecipe.bind(this);
+    this.setName = this.setName.bind(this);
+>>>>>>> Stashed changes
   }
 
   changeView(view) {
@@ -36,6 +54,15 @@ class App extends React.Component {
     this.changeView('overview');
   }
 
+
+  responseGoogle(response) {
+    console.log(response);
+    console.log(this);
+    this.setName(response);
+    console.log(this.state.fullName);
+  }
+  
+
   renderComponent() {
     if (this.state.view === 'overview') {
       return <Recipe recipe={this.state.recipe}/>
@@ -49,17 +76,37 @@ class App extends React.Component {
     }
   }
 
+  setName(name) {
+    this.setState({
+      name: name
+    })
+  }
+
   render() {
     return (
       <div>
-        <div>
-        Some Freaking Awesome Scrolling Thingy
-        </div>
+        <GoogleLogin
+          clientId="1092722979964-8pjuqqchc7d0qlfh0iaeisqkjp2bb1f1.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
+        <h3>Welcome {this.state.fullName}</h3>
         <ul>
+<<<<<<< Updated upstream
           <li><a onClick={() => this.changeView('')}>Home</a></li>
           <li><a onClick={() => this.changeView('add')}>Create</a></li>
           <li><a>Logout</a></li>
+=======
+          <li><a>Home</a></li>
+          <li><a onClick={() => this.addRecipe()}>Create</a></li>
+>>>>>>> Stashed changes
         </ul>
+        <GoogleLogout
+          buttonText="Logout"
+          onLogoutSuccess={this.logout}
+        >
+        </GoogleLogout>
         <div>
         {this.renderComponent()}
         </div>
