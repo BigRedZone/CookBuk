0b4558977a7bd8bd18784e3f04f785ad20241da7
Nathan Ong <nathan.d.ong@gmail.com>
2018-09-12 11:07:16 -0700

    Update port to include heroku generated port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //server
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 const db = require('../database/index.js');
