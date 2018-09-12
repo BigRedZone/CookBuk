5f0fb08786a1f7b1bec324e73a2f0655b4740360
tae-lim <taelim92@gmail.com>
2018-09-12 11:57:33 -0700

    setup get request for recipes in selection component

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -1,21 +1,60 @@
 import React from 'react';
+import $ from 'jquery';
 
-const Selection = (props) => {
-  return (
-    <div>
-      <h3>Welcome {props.user.split(' ')[0]}</h3>
-      {
-        props.recipes.map((recipe) => 
-          <div>
-          <h1 onClick={() => props.selectRecipe(recipe)}>{recipe.name}</h1>
-          <span>{recipe.prepTime}</span>
-          <span>{recipe.cookTime}</span>
-          <span>{recipe.servings}</span>
-          </div>
-        )
-      }
-    </div>
-  );
+class Selection extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: []
+    }
+    this.fetch = this.fetch.bind(this);
+    this.setRecipes = this.setRecipes.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetch('/recipes', this.setRecipes);
+  }
+
+  setRecipes(data) {
+    this.setState({
+      recipes: data
+    });
+  }
+
+  fetch(url, callback) {
+    console.log(this);
+    $.ajax({
+      type: 'GET',
+      url: url
+    })
+    .done((data) => {
+      callback(data);
+      console.log('GET SUCCESS');
+    })
+    .fail(() => {
+      console.log('GET FAILED');
+    });
+  }
+  
+  render () {
+    console.log('props', this.props);
+    return (
+      <div>
+        <h3>Welcome {this.props.user.split(' ')[0]}</h3>
+        {
+          this.props.recipes.map((recipe) => 
+            <div>
+            <h1 onClick={() => this.props.selectRecipe(recipe)}>{recipe.name}</h1>
+            <span>{recipe.prepTime}</span>
+            <span>{recipe.cookTime}</span>
+            <span>{recipe.servings}</span>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default Selection;
