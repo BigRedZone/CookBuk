64ecbf37673eb4f5ef9c0abfa54ccfb20072d883
tae-lim <taelim92@gmail.com>
2018-09-12 14:02:00 -0700

    query recipes by username, render recipes to Selection component

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -4,26 +4,14 @@ import $ from 'jquery';
 class Selection extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      recipes: []
-    }
     this.post = this.post.bind(this);
-    this.setRecipes = this.setRecipes.bind(this);
   }
 
   componentDidMount() {
-    this.post('/recipes', this.setRecipes);
-  }
-
-  setRecipes(data) {
-    this.setState({
-      recipes: data
-    });
+    this.post('/recipes', this.props.setRecipes);
   }
 
   post(url, callback) {
-    console.log('this in fetch', this);
     $.ajax({
       type: 'POST',
       url: url,
@@ -33,17 +21,14 @@ class Selection extends React.Component {
     })
     .done((data) => {
       callback(data);
-      console.log('GET SUCCESS');
-      console.log('this successful data', data);
-      console.log('this is recipes', this.state.recipes)
+      console.log('POST: Successfully retrieved Recipes');
     })
     .fail(() => {
-      console.log('GET FAILED');
+      console.log('POST: Failed to retrieve Recipes');
     });
   }
   
   render () {
-    console.log('props', this.props);
     return (
       <div>
         <h3>Welcome {this.props.user.split(' ')[0]}</h3>
