b11e8d22136aa654c739db6917cc02c9d2332122
Nathan Ong <nathan.d.ong@gmail.com>
2018-09-14 14:41:59 -0700

    Add CORS header middleware

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const bodyParser = require('body-parser');
 
 const db = require('../database/index.js');
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
